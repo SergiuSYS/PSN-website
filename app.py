f218646639eec43ab0d5add76224fd5da994514e
Sergiu <sergiusay06@gmail.com>
2026-05-21 15:55:10 +0300

    a

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ import threading
 
 app = flask.Flask(__name__)
 
+
 recived_data = {
     "temperature": 0,
     "humidity": 0,
-    "flood": 0,
-    "alerts": [],
-    "messages": []
+    "flood": 0,  
+    "alerts":[],
+    "messages":[]
 }
 
 commands_to_send = {
     "r": 0,
     "g": 0,
     "b": 0,
-    "message": None
+    "message":None
 }
 
 @app.route('/')
@@ -26,10 +27,12 @@ def home():
 
 @app.route('/data')
 def get_data():
+    global recived_data
     return flask.jsonify(recived_data)
 
 @app.route('/led', methods=['POST'])
 def send_led_color():
+    global commands_to_send
     data = flask.request.json
     commands_to_send['r'] = data['r']
     commands_to_send['g'] = data['g']
@@ -38,24 +41,35 @@ def send_led_color():
 
 @app.route('/send-msg', methods=['POST'])
 def send_message():
+    global commands_to_send
     data = flask.request.json
     commands_to_send["message"] = data["message"]
     return "OK"
 
 def server():
+    global client
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 9999))
     server.listen(1)
 
     while True:
+        global recived_data
         client, addr = server.accept()
         raw_data = client.recv(1024).decode()
         data = json.loads(raw_data)
 
-        recived_data.update(data)
+        recived_data['temperature'] = data['temperature']
+        recived_data['humidity'] = data['humidity']
+        recived_data['flood'] = data['flood'] 
+        recived_data['messages'] = data['messages']
+        recived_data['alerts'] = data['alerts']
+        
+        print(data['temperature'])
 
         client.send(json.dumps(commands_to_send).encode())
         commands_to_send["message"] = None
         client.close()
 
-threading.Thread(target=server, daemon=True).start()
+threading.Thread(target=server,daemon=True).start()
+
+app.run(host="0.0.0.0", port=5000)
